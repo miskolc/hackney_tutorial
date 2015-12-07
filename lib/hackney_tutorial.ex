722c81f6016cbc50a089027ed2c40c0eabfd9e41
Dragos C <calinho_gx1992@yahoo.com>
2015-12-07 08:28:54 +0200

    Add default arguments

--- lib/hackney_tutorial.ex
+++ lib/hackney_tutorial.ex
@@ -1,10 +1,6 @@
 defmodule HackneyTutorial do
-  def simple_request do
-    method = :get
-    url = <<"https://friendpaste.com">>
-    headers = []
-    payload = <<>>
-    options = []
+  def simple_request(method \\ :get, url \\ "https://friendpaste.com",
+                     headers \\ [], payload \\ "", options \\ []) do
     {:ok, status, responseHeaders, clientReference} = :hackney.request(method, url, headers,
                                                                        payload, options )
   end
